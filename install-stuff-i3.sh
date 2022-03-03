b5027f6907c93637d58a53b85f6ad42f7cf5b1c2
Meinhoff <100754218+Meinhoff@users.noreply.github.com>
2022-03-03 12:18:38 +0100

    Update install-stuff-i3.sh

--- install-stuff-i3.sh
+++ install-stuff-i3.sh
@@ -1,3 +1,7 @@
 sudo pacman -S ntfs-3g gnome-disk-utility pulseaudio pulseaudio-alsa pavucontrol xorg xorg-xinit xorg-server-devel xorg-server bash-completion firefox neofetch base-devel go reflector git make flatpak gcc fakeroot archlinux-keyring chromium nemo nemo-fileroller nitrogen ttf-ubuntu-font-family ttf-font-awesome noto-fonts-emoji flameshot curl zsh zsh-completions dmenu rxvt-unicode xfce4-terminal rofi picom lm_sensors i3-gaps i3status i3lock i3blocks
 sudo pacman -Syyu
 sudo reflector --country Sweden  --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+mkdir .config/rofi
+mkdir .config/rofi/themes
+cp config.rasi .config/rasi
+cp fancy.rasi .config/rasi/themes
