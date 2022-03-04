99700c48264aebe36086ca95293036078d169ea8
Meinhoff <100754218+Meinhoff@users.noreply.github.com>
2022-03-04 15:49:08 +0100

    Update grundinstallation.sh

--- grundinstallation.sh
+++ grundinstallation.sh
@@ -1,4 +1,4 @@
-sudo pacman -S ntfs-3g gnome-disk-utility pulseaudio neofetch pulseaudio-alsa pavucontrol xorg xorg-xinit xorg-server-devel xorg-server bash-completion firefox neofetch base-devel go reflector git make flatpak gcc fakeroot archlinux-keyring chromium nemo nemo-fileroller nitrogen ttf-ubuntu-font-family ttf-font-awesome noto-fonts-emoji flameshot curl zsh zsh-completions dmenu rxvt-unicode xfce4-terminal rofi picom lm_sensors i3-gaps i3status i3lock i3blocks alsa-utils pulsemixer signal-desktop
+sudo pacman -S ntfs-3g gnome-disk-utility pulseaudio neofetch pulseaudio-alsa pavucontrol xorg xorg-xinit xorg-server-devel xorg-server bash-completion firefox neofetch base-devel go reflector git make flatpak gcc fakeroot archlinux-keyring chromium nemo nemo-fileroller nitrogen ttf-ubuntu-font-family ttf-font-awesome noto-fonts-emoji flameshot curl zsh zsh-completions dmenu rxvt-unicode xfce4-terminal rofi picom lm_sensors i3-gaps i3status i3lock i3blocks alsa-utils pulsemixer signal-desktop darktable kate
 mkdir $HOME/.config/rofi
 mkdir $HOME/.config/rofi/themes
 cp config.rasi $HOME/.config/rofi
