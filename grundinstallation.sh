c8b8f23fc42b690352ebe854b43c93f7c7f2bc59
Meinhoff <100754218+Meinhoff@users.noreply.github.com>
2022-03-04 10:34:49 +0100

    Update and rename install-stuff-i3.sh to grundinstallation.sh

--- grundinstallation.sh
+++ grundinstallation.sh
@@ -1,4 +1,4 @@
-sudo pacman -S ntfs-3g gnome-disk-utility pulseaudio pulseaudio-alsa pavucontrol xorg xorg-xinit xorg-server-devel xorg-server bash-completion firefox neofetch base-devel go reflector git make flatpak gcc fakeroot archlinux-keyring chromium nemo nemo-fileroller nitrogen ttf-ubuntu-font-family ttf-font-awesome noto-fonts-emoji flameshot curl zsh zsh-completions dmenu rxvt-unicode xfce4-terminal rofi picom lm_sensors i3-gaps i3status i3lock i3blocks alsa-utils pulse-mixer
+sudo pacman -S ntfs-3g gnome-disk-utility pulseaudio neofetch pulseaudio-alsa pavucontrol xorg xorg-xinit xorg-server-devel xorg-server bash-completion firefox neofetch base-devel go reflector git make flatpak gcc fakeroot archlinux-keyring chromium nemo nemo-fileroller nitrogen ttf-ubuntu-font-family ttf-font-awesome noto-fonts-emoji flameshot curl zsh zsh-completions dmenu rxvt-unicode xfce4-terminal rofi picom lm_sensors i3-gaps i3status i3lock i3blocks alsa-utils pulsemixer
 mkdir $HOME/.config/rofi
 mkdir $HOME/.config/rofi/themes
 cp config.rasi $HOME/.config/rofi
@@ -7,12 +7,10 @@ mkdir $HOME/.config/i3blocks
 mkdir $HOME/.config/i3blocks/scripts
 cp cpu_usage disk memory shutdown_menu volume wifi $HOME/.config/i3blocks/scripts/
 cp i3blocksconfig $HOME/.config/i3blocks/
-cp config $HOME/.config/i3/
 mv $HOME/.config/i3blocks/i3blocksconfig $HOME/.config/i3blocks/config
 chmod +x $HOME/.config/i3blocks/scripts/*
 mkdir $HOME/Wallpapers
 cp mount1.jpg $HOME/Wallpapers
-rm -rf Linuxgrejer
 echo "neofetch" >> $HOME/.bashrc
 sudo pacman -S nvidia nvidia-utils nvidia-settings
 sudo nvidia-xconfig
