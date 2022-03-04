ad1fa8ba33fc733c0d95678b7c1711fc1ff5b339
Meinhoff <100754218+Meinhoff@users.noreply.github.com>
2022-03-04 15:53:31 +0100

    Update fixa-config.sh

--- fixa-config.sh
+++ fixa-config.sh
@@ -3,3 +3,4 @@ cp config $HOME/.config/i3/
 nitrogen --set-zoom-fill $HOME/Wallpapers/mount1.jpg
 mkdir $HOME/.config/picom
 cp picom.conf $HOME/.config/picom/
+echo "Klart. Tryck Windows + shift + c och sen Windows + shift + r"
