a715bfac8c0400215e10ccab339501b75ac559ea
Meinhoff <100754218+Meinhoff@users.noreply.github.com>
2022-03-04 10:12:58 +0100

    Update install-stuff-i3.sh

--- install-stuff-i3.sh
+++ install-stuff-i3.sh
@@ -1,6 +1,4 @@
 sudo pacman -S ntfs-3g gnome-disk-utility pulseaudio pulseaudio-alsa pavucontrol xorg xorg-xinit xorg-server-devel xorg-server bash-completion firefox neofetch base-devel go reflector git make flatpak gcc fakeroot archlinux-keyring chromium nemo nemo-fileroller nitrogen ttf-ubuntu-font-family ttf-font-awesome noto-fonts-emoji flameshot curl zsh zsh-completions dmenu rxvt-unicode xfce4-terminal rofi picom lm_sensors i3-gaps i3status i3lock i3blocks alsa-utils pulse-mixer
-sudo pacman -Syyu
-sudo reflector --country Sweden  --age 12 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 mkdir $HOME/.config/rofi
 mkdir $HOME/.config/rofi/themes
 cp config.rasi $HOME/.config/rofi
@@ -18,5 +16,5 @@ rm -rf Linuxgrejer
 echo "neofetch" >> $HOME/.bashrc
 sudo pacman -S nvidia nvidia-utils nvidia-settings
 sudo nvidia-xconfig
-echo "Klaaaaart! Boota om data-apparaturen! Logga in med avändaren och skriv startx"
+echo "Klaaaaart! Boota om data-apparaturen!"
 
