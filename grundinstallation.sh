87bc9cb04e2348011ba92ba6413110a028c40ceb
Meinhoff <100754218+Meinhoff@users.noreply.github.com>
2022-03-04 12:47:03 +0100

    Update grundinstallation.sh

--- grundinstallation.sh
+++ grundinstallation.sh
@@ -1,4 +1,4 @@
-sudo pacman -S ntfs-3g gnome-disk-utility pulseaudio neofetch pulseaudio-alsa pavucontrol xorg xorg-xinit xorg-server-devel xorg-server bash-completion firefox neofetch base-devel go reflector git make flatpak gcc fakeroot archlinux-keyring chromium nemo nemo-fileroller nitrogen ttf-ubuntu-font-family ttf-font-awesome noto-fonts-emoji flameshot curl zsh zsh-completions dmenu rxvt-unicode xfce4-terminal rofi picom lm_sensors i3-gaps i3status i3lock i3blocks alsa-utils pulsemixer
+sudo pacman -S ntfs-3g gnome-disk-utility pulseaudio neofetch pulseaudio-alsa pavucontrol xorg xorg-xinit xorg-server-devel xorg-server bash-completion firefox neofetch base-devel go reflector git make flatpak gcc fakeroot archlinux-keyring chromium nemo nemo-fileroller nitrogen ttf-ubuntu-font-family ttf-font-awesome noto-fonts-emoji flameshot curl zsh zsh-completions dmenu rxvt-unicode xfce4-terminal rofi picom lm_sensors i3-gaps i3status i3lock i3blocks alsa-utils pulsemixer signal-desktop
 mkdir $HOME/.config/rofi
 mkdir $HOME/.config/rofi/themes
 cp config.rasi $HOME/.config/rofi
@@ -14,5 +14,5 @@ cp mount1.jpg $HOME/Wallpapers
 echo "neofetch" >> $HOME/.bashrc
 sudo pacman -S nvidia nvidia-utils nvidia-settings
 sudo nvidia-xconfig
-echo "Klaaaaart! Boota om data-apparaturen!"
+echo "Del 1 klar. Boota om datamaskin-apparaturen!" 
 
